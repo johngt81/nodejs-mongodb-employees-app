6830fb70b4102f72fed6f2d7ef5b9df67671e9f5
John Bryan Gaona Trinidad <john.gt81@gmail.com>
2024-07-08 16:23:06 -0500

    use jwt for authentication

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # nodejs-mongodb-employees-app
+
+
+Command for MongoDB
+
+To connect to mongo with an authenticated user
+
+mongosh --username user --password pass
+
+use my_db
+db.createCollection("my_collection")
+db.my_collection.insertOne({});
+db.my_collection.find();
+
+https://www.mongodb.com/docs/mongodb-shell/
--- customer_app.js
+++ customer_app.js
@@ -6,6 +6,11 @@ const bcrypt = require('bcrypt');
 const saltRounds = 5;
 const session = require('express-session');
 const uuid = require('uuid');
+const Employees = require('./employee');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const secretKey = 'mySecretKey';
+let usersDictionary = {};
 
 const app = express();
 
@@ -19,7 +24,7 @@ app.use("*", bodyParser.json());
 
 //Middleware to handle URL-encoded form data
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(cors());
 app.use(session({
     cookie: {maxAge: 120000}, //session expires after 2 minutes
     secret: 'mysecret',
@@ -29,34 +34,87 @@ app.use(session({
 }));
 
 //Endpoints
-app.get('/', (req, res) => {
+app.get('/', (req, verifyToken, res) => {
     res.send('Heeelello')
 })
 
-app.post('api/login', async (req, res) => {
+app.post('/api/login', async (req, res) => {
     const data = req.body;
-    let user_name = data['user_name'];
+    let username = data['user_name'];
     let password = data['password'];
 
-    const documents = await Customers.find({
-        user_name: user_name,
-        password: password
-    });
-
-    if (documents.length > 0){
-        const storedPassword = documents[0]['password'];
-        let result = await bcrypt.compare(password, storedPassword);
-        if (result){
-            const genidValue = req.sessionID;
-            res.cookie('username', user_name);
-            res.send('User Logged In');
-        } else {
-            res.send("Incorrect");
-        }
-    } else {
-        res.send("User information incorrect");
+    const user = usersDictionary[username];
+    const passwordMatched = await bcrypt.compare(password, user.hashedpwd);
+    if (!user || !passwordMatched){
+        res.status(401).send('User information incorrect');
+        return;
     }
+    res.status(200).send('Login successfully');
+    // const documents = await Customers.find({
+    //     user_name: user_name
+    // });
+
+    // if (documents.length > 0){
+    //     const user = documents[0];
+
+    //     const token = jwt.sign({
+    //         username: user.user_name 
+    //      }, secretKey);
+    //      res.json({token});
+
+    //     const storedPassword = documents[0]['password'];
+
+    //     let hashedpwd = bcrypt.hashSync(password, saltRounds);
+
+    //     let result = await bcrypt.compare(hashedpwd, storedPassword);
+    //     // let result = await bcrypt.compare(password, storedPassword);
+    //     if (result){
+    //         const genidValue = req.sessionID;
+    //         res.cookie('username', user_name);
+    //         res.send('User Logged In');
+    //     } else {
+    //         res.send("Incorrect");
+    //     }
+    // } else {
+    //     res.send("User information incorrect");
+    // }
 })
+
+const authenticationToken = (req, res, next) => {
+    const token = req.headers['authorization'];
+
+    if (!token){
+        res.sendStatus(401);
+        return;
+    }
+
+    jwt.verify(token, secretKey, (err, user) => {
+        if (err){
+            res.sendStatus(403);
+            return;
+        }
+
+        req.user = user;
+        next();
+    })
+}
+
+function verifyToken(req, res, next){
+    const token = req.headers['authorization'];
+    
+    if (typeof token !== 'undefined'){
+        jwt.verify(token, secretKey, (err, authData) => {
+            if (err){
+                res.sendStatus(403);
+            } else {
+                res.authData = authData;
+                next();
+            }
+        });
+    } else {
+        res.sendStatus(401);
+    }
+}
 
 app.get('/api/logout', async (req, res) => {
     req.session.destroy((err) => {
@@ -69,30 +127,52 @@ app.get('/api/logout', async (req, res) => {
     })
 });
 
-app.post('api/add_customer', async (req, res) => {
+app.post('/api/add_customer', async (req, res) => {
     const data = req.body;
+    const username = data['user_name'];
 
     const documents = await Customers.find({
-        user_name: data['user_name'],
+        user_name: username,
     });
 
     if (documents.length > 0){
-        res.send('User already exists');
+        res.status(409).send('User already exists');
+        return;
     }
 
-    let hashedpwd = bcrypt.hashSync(data['password'], saltRounds);
+    const hashedpwd = await bcrypt.hash(data['password'], saltRounds);
+    usersDictionary[username] = { hashedpwd };
+
+    //Creating a customer with password hashed
     const customer = new Customers({
-        "user_name": data['user_name'],
+        "user_name": username,
         "age": data['age'],
         "password": hashedpwd,
-        // "password": data['password'],
         "email": data['email']
     });
 
     await customer.save();
-    res.send('Customer added successfully');
+
+    res.status(201).send('Customer added successfully');
 })
 
+app.get('/api/employees', async (req, res) => {
+    const documents = await Employees.find();
+    res.json(documents);
+});
+
+app.post('/api/add_employee', async (req, res)=> {
+    const data = req.body;
+    const emp = new Employees({
+        "emp_name": data["name"],
+        "age": data["age"],
+        "location": data["location"],
+        "email": data["email"]
+    });
+
+    await emp.save();
+    res.json({ message: "Employee added." })
+})
 
 
 app.listen(port, () => {
